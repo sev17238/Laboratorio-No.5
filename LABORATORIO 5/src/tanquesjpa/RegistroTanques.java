776ba32fdc1e6c7196e14f5d6a5773aa59f611f9
SDiego <SDiego@SDiego-PC>
2017-11-01 23:40:58 -0600

    Primer commit: Cosas hechas: - Librerias para persistencia agregadas. - Clase RegistroTanques agregada.
    Cosas faltantes: 
    - Crear base de datos "tanques" para conectar al servidor y accesar
    a ella desde netbeans.
    - Agregar clase para excepciones.

---
+++ LABORATORIO 5/src/tanquesjpa/RegistroTanques.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tanquesjpa;
+
+/**
+ *
+ * @author SDiego
+ */
+public class RegistroTanques {
+    private Tanque[] tanquesRegion;
+    
+    public RegistroTanques(){
+        tanquesRegion = new Tanque[10];
+    }
+    
+}
